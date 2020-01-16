a9c425a9331d5069665df16b8954fb42afb6c22b
kaurgur99 <kaur7225@sheridancollege.ca>
2020-01-16 10:52:08 -0500

    Message

---
+++ src/ThirsdayExample.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kaur Gurleen
+ */
+public class ThirsdayExample {
+    public static void main(String[]args){
+        System.out.println("Learning GItHub");
+    }
+}
